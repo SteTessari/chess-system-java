41bdf38a3990806a1c9d61ad27fb032933efb65f
SteTessari <stephanietessare@gmail.com>
2021-12-11 15:21:00 -0300

    Little improvement in board printing

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -5,9 +5,9 @@ public class Position {
 	private int row;
 	private int column;
 	
-	public Position(int row, int colum) {
+	public Position(int row, int column) {
 		this.row = row;
-		this.column = colum;
+		this.column = column;
 	}
 
 	public int getRow() {
@@ -18,12 +18,12 @@ public class Position {
 		this.row = row;
 	}
 
-	public int getColum() {
+	public int getColumn() {
 		return column;
 	}
 
-	public void setColum(int colum) {
-		this.column = colum;
+	public void setColum(int column) {
+		this.column = column;
 	}
 	
 	@Override
@@ -31,7 +31,7 @@ public class Position {
 		return row + ", " + column;
 	}
 	
-	public void setValues(int row, int colum) {
+	public void setValues(int row, int column) {
 		
 	}
 }
